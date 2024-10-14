780ba96e404065ba98065af252a299d337c92686
Kits <luck3801@gmail.com>
2024-10-14 10:19:58 -0300

    feat: added content to function

--- updateComission.js
+++ updateComission.js
@@ -1,6 +1,8 @@
-let cards = document.querySelectorAll('CardView-relatoria');
-let comissao = apex.item("P2_COMISSAO");
+function updateRelatoria() {
+  let cards = document.querySelectorAll('CardView-relatoria');
+  let comissao = apex.item("P2_COMISSAO");
 
-cards.forEach((element) => {
-  console.log(element.innerHTML);
-})
+  cards.forEach((element) => {
+    console.log(element.innerHTML);
+  })
+}
